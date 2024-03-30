54603fff3348c5ab56778c0fe8245c032c47320e
Aadarsh Dangi <78791785+LegXgodGaming@users.noreply.github.com>
2024-03-31 00:56:53 +0530

    finally
    finally

--- day22 holi/script.js
+++ day22 holi/script.js
@@ -1,13 +1,52 @@
+function init() {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector(".main"),
+        smooth: true
+    });
+    locoScroll.on("scroll", ScrollTrigger.update);
+
+    ScrollTrigger.scrollerProxy(".main", {
+        scrollTop(value) {
+            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+        },
+        getBoundingClientRect() {
+            return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+        },
+        pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
+    });
+
+
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+    ScrollTrigger.refresh();
+
+}
+
+init() 
+ 
+ 
  let images = [
-    'url(" https://zeevector.com/wp-content/uploads/2021/02/Black-Color-Splash-PNG-768x699.png ")',
-    'url("  ")',
-    'url("  ")',
-    'url("  ")',
-    'url("  ")',
-    'url("  ")',
-    'url("  ")',
-    'url("  ")',
-    'url("  ")',
+    
+    'url("images/color1.png ")',
+    'url("images/color2.png ")',
+    'url("images/color3.png ")',
+    'url("images/color4.png ")',
+    'url("images/color5.png ")',
+    'url("images/color6.png ")',
+    'url("images/color7.png ")',
+    'url("images/color8.png ")',
+    'url("images/color9.png ")',
+    'url("images/color10.png ")',
+    'url("images/color11.png ")',
+    'url("images/color12.png ")',
+    'url("images/color13.png ")',
+    'url("images/color14.png ")',
+    'url("images/color15.png ")',
+    'url("images/color16.png ")',
+
+  
 
  ]
 
@@ -31,3 +70,5 @@ page1.onclick = function(e){
         splash.remove()
     }, 4000);
 }
+
+
